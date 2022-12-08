34d66958ff8185814e64fd503db1a4c056ee1251
Abhishek Rao Chimbili <abhishekraochimbili@gmail.com>
2022-12-08 15:30:48 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    big = 10000
+
+    for i in range(2, big):
+        for x in range(2, i):
+            if(i%x == 0):
+                break
+        else:
+            if(len(list) < number_of_primes):
+                list.append(i)
+
+        
+
+
     return list
